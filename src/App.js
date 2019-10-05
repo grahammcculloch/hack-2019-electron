aeae67856888c58e6052d7cc481b6afe5efb43ad
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-10-05 11:57:45 +0700

    Round trip comms working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from '@blueprintjs/core';
 import './App.scss';
+const { ipcRenderer, remote } = window.require('electron');
+const { dialog } = remote;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      processing: false,
+      textFile: '',
+      audioFile: '',
+      timingFile: '',
+      backgroundFile: '',
+      outputFile: '',
+    };
+    ipcRenderer.on('did-finish-conversion', (event, args) => {
+      console.log('Received result', args)
+      this.setState({ processing: false, outputFile: args.outputFile });
+    });
+  }
+
+  selectTextFile = () => {
+    const textFiles = dialog.showOpenDialog({
+      properties: ['openFile'],
+    });
+    if (textFiles && textFiles.length === 1) {
+      this.setState({ textFile: textFiles[0] });
+    }
+  };
+
+  onGo = () => {
+    this.setState({ processing: true }, () => {
+      const { textFile, audioFile, timingFile, backgroundFile } = this.state;
+      const args = {
+        textFile,
+        audioFile,
+        timingFile,
+        backgroundFile,
+      };
+      console.log('Requesting processing', args);
+      ipcRenderer.send('did-start-conversion', args);
+    });
+  };
+
   render() {
+    const { processing, textFile, outputFile } = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
+      <div className='App bp3-dark'>
+        <div>
+          <span>Select text file: </span>
+          <Button text='Select' onClick={this.selectTextFile} />
+          <span>{textFile}</span>
         </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
+        <Button loading={processing} text='Go!' onClick={this.onGo} />
+        <span>{outputFile}</span>
       </div>
     );
   }
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 // Module to control application life.
-const app = electron.app;
+const { app, ipcMain } = electron;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
@@ -12,48 +12,69 @@ const url = require('url');
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: { nodeIntegration: true }});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: true },
+  });
 
-    // and load the index.html of the app.
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-            pathname: path.join(__dirname, '/../build/index.html'),
-            protocol: 'file:',
-            slashes: true
-        });
-    mainWindow.loadURL(startUrl);
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  // and load the index.html of the app.
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, '/../build/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    });
+  mainWindow.loadURL(startUrl);
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+function handleSubmission() {
+  ipcMain.on('did-start-conversion', (event, args) => {
+    console.log('Starting command line', args);
+    setTimeout(() => {
+      const retArgs = { outputFile: 'output.webm' };
+      console.log('Command line process finished', retArgs);
+      event.sender.send('did-finish-conversion', retArgs);
+    }, 2000);
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+app.on('ready', () => {
+  console.log('Ready!');
+  createWindow();
+  handleSubmission();
 });
 
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
